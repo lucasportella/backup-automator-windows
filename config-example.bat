e7357a5707075ec06187383298945ee3d6994933
Lucas Portella <lucasportellaagu@gmail.com>
2024-01-08 11:20:15 -0300

    feat: config-example update more variables

--- config-example.bat
+++ config-example.bat
@@ -2,8 +2,7 @@
 
 REM Credentials
 set "system_user=mike"
-set "samba_user=john" REM not best practice, samba_user should be the same as system_user
-set "samba_password=password"
+set "system_password=password"
 
 REM Log directory
 set "log_dir=C:\Users\%system_user%\backup\logs"
@@ -14,7 +13,6 @@ set "shared_dir_source=\\192.168.0.10\driveName"
 set "shared_dir_mount_target=C:\Users\%system_user%\backup\shared-folder"
 
 REM Destination directory
-set "destination_mount_device_location=\\?\Volume{Volume GUID}"
 set "destination_dir=C:\Users\%system_user%\backup\%system_user%-destination-backup\my-backup"
 
 echo Variables set successfully.
