d2edbc30c6f90bf146638e0f5137c857027438d2
Lucas Portella <lucasportellaagu@gmail.com>
2024-01-08 11:21:05 -0300

    feat: backup-script manage directories creation

--- backup-script.bat
+++ backup-script.bat
@@ -1,13 +1,33 @@
 echo Starting backup script...
 call config.bat
 
-rem Check if the network drive exists
+rem Source directory
 if not exist %networkDrive%\ (
     echo Mapping %networkDrive% to %shared_dir_source%
     net use %networkDrive% %shared_dir_source% /user:%system_user% %system_password%
 ) else (
     echo %networkDrive% is already mapped
 )
+
+rem Destination directory
+if exist %destination_dir% (
+    echo "Destination directory already exists."
+) else (
+    mkdir %destination_dir%
+)
+
+
+
+rem Log directory
+if exist %log_dir% (
+    echo "Log directory already exists."
+) else (
+    mkdir %log_dir%
+)
+
+echo Variables created.
+
+
 
 
 REM Unmap the network drive when done (optional)
